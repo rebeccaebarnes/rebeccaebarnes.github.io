dc4a80281b06b051fe4d383f477f13c9e3019791
Rebecca Barnes <rebeccaebarnes@gmail.com>
2019-02-08 09:56:19 -0800

    feat: Add code for displaying plotly plots to routes.py

--- webapp/worldbankapp/routes.py
+++ webapp/worldbankapp/routes.py
@@ -1,8 +1,20 @@
-from worldbankapp import app
+import json
 
+import plotly
 from flask import render_template
+
+from worldbankapp import app
+from wrangling_scripts.wrangle_data import return_figures
 
 @app.route("/")
 @app.route("/index")
 def index():
-    return render_template("index.html")
+    figures = return_figures()
+
+    # plot ids for the html id tag
+    ids = ['figure-{}'.format(i) for i, _ in enumerate(figures)]
+
+    # Convert the plotly figures to JSON for javascript in html template
+    figuresJSON = json.dumps(figures, cls=plotly.utils.PlotlyJSONEncoder)
+
+    return render_template("index.html", ids=ids, figuresJSON=figuresJSON)
