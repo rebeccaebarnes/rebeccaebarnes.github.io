4fabc0cfa08c760dc8853023dba71a9de73772e5
Rebecca Barnes <rebeccaebarnes@gmail.com>
2019-02-09 13:46:42 -0800

    feat: Implement settings for Heroku hosting

--- webapp/worldbank.py
+++ webapp/worldbank.py
@@ -1,3 +1,1 @@
 from worldbankapp import app
-# For details on host address: https://stackoverflow.com/questions/46835568/this-site-can-t-be-reached-flask-python
-app.run(host="127.0.0.1", debug=True)
